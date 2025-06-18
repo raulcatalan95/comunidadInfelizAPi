622acf9a2cf688101afbf1db08c99c573b5c37f9
Raul Catalan <raulcatalan95@gmail.com>
2025-06-18 19:56:10 -0400

    first commit

---
+++ src/main/java/cl/comunidadinfeliz/ComunidadinfelizApplication.java
@@ -0,0 +1,13 @@
+package cl.comunidadinfeliz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunidadinfelizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComunidadinfelizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/comunidadinfeliz/ComunidadinfelizApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.comunidadinfeliz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunidadinfelizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
