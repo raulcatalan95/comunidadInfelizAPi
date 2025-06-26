a2ab7cbf6f6358761d27bd18f57a3cbbec0a7e3a
Bastian <bastiancampusanode@gmail.com>
2025-06-26 09:13:03 -0400

    feat: mensaje en /

--- src/main/java/cl/comunidadinfeliz/ComunidadinfelizApplication.java
+++ src/main/java/cl/comunidadinfeliz/ComunidadinfelizApplication.java
@@ -2,12 +2,19 @@ package cl.comunidadinfeliz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ComunidadinfelizApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ComunidadinfelizApplication.class, args);
-	}
-//comentario
+    public static void main(String[] args) {
+        SpringApplication.run(ComunidadinfelizApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Comunidad Infeliz API está corriendo correctamente.";
+    }
 }
